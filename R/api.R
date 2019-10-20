8d0522e57e12c2b35f41b61fc9a197862933ce05
ramusz1 <ramusz984@gmail.com>
2019-10-20 02:10:23 +0200

    Added missing endpoints

--- R/api.R
+++ R/api.R
@@ -26,3 +26,79 @@ get_item_by_id <- function(id) {
   request_url <- create_request_url(.base_url(), c("item", id))
   .send_request(request_url)
 }
+
+#' Get user corresponding to the specified username
+#'
+#' @param username of the user that should be retrieved
+#'
+#' @return user corresponding to the specified username
+#'
+get_user_by_username <- function(username) {
+  request_url <- create_request_url(.base_url(), c("user", username))
+  .send_request(request_url)
+}
+
+#' Get the current largest item id
+#'
+#' @return current largest item id
+get_max_item_id <- function() {
+  request_url <- create_request_url(.base_url(), c("maxitem"))
+  .send_request(request_url)
+}
+
+#' Get top stories
+#'
+#' @return list of top stories ids
+get_top_stories <- function() {
+  request_url <- create_request_url(.base_url(), c("topstories"))
+  .send_request(request_url)
+}
+
+#' Get new stories
+#'
+#' @return list of new stories ids
+get_new_stories <- function() {
+  request_url <- create_request_url(.base_url(), c("newstories"))
+  .send_request(request_url)
+}
+
+#' Get best stories
+#'
+#' @return list of best stories ids
+get_best_stories <- function() {
+  request_url <- create_request_url(.base_url(), c("beststories"))
+  .send_request(request_url)
+}
+
+#' Get latest ask HN stories
+#'
+#' @return list of latest ask HN stories ids
+get_latest_ask_stories <- function() {
+  request_url <- create_request_url(.base_url(), c("askstories"))
+  .send_request(request_url)
+}
+
+#' Get latest show HN stories
+#'
+#' @return list of latest show HN stories ids
+get_latest_show_stories <- function() {
+  request_url <- create_request_url(.base_url(), c("showstories"))
+  .send_request(request_url)
+}
+
+#' Get latest job stories
+#'
+#' @return list of latest job stories ids
+get_latest_job_stories <- function() {
+  request_url <- create_request_url(.base_url(), c("jobstories"))
+  .send_request(request_url)
+}
+
+
+#' Get changed items and profiles
+#'
+#' @return list of changed item ids and list of changed profile usernames
+get_updates <- function() {
+  request_url <- create_request_url(.base_url(), c("updates"))
+  .send_request(request_url)
+}
