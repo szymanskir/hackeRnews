4ae2a2ce53812314fb619d38fb311d0f1ae8bf7b
Piotr <piotrek.janus96@gmail.com>
2019-10-24 23:16:49 +0200

    Retrieve items for given ids, functions with sufix _ids added

--- R/api.R
+++ R/api.R
@@ -16,6 +16,41 @@
   get_content(hn_api_response)
 }
 
+#' Get item coresponding to given id and preprocess it's content
+#'
+#' @param id id of the item that should be retrieved
+#'
+#' @return list containing API respone
+#'
+.get_item <- function(id) {
+  items <- get_item_by_id(id)
+  class(items) <- 'hackerlist'
+  items$time <- as.POSIXct(items$time, origin="1970-01-01")
+  items
+}
+
+#' Print values for "hackerlist" type objects
+#'
+#' @param x "hackerlist" type list
+#' @param ... further arguments passed to or from other methods
+#'
+#' @export
+print.hackerlist <- function(x, ...) {
+  cat('Title:', x$title, '\n')
+  cat('ID:', x$id, '\n')
+  cat('Author:', x$by, '\n')
+  cat('Descendants:', x$descendants, '\n')
+  cat('Kids: ')
+  if(length(x$kids) > 5)
+    cat(utils::head(x$kids),'... \n')
+  else
+    cat(x$kids, '\n')
+  cat('Score:', x$score,'\n')
+  cat('Date:', as.character(x$time),'\n')
+  cat('Type:', x$type,'\n')
+  cat('URL:', x$url,'\n')
+}
+
 #' Retrieves the item corresponding to the specified id
 #'
 #' @export
@@ -28,6 +63,7 @@ get_item_by_id <- function(id) {
   request_url <- create_request_url(.base_url(), c("item", id))
   .send_request(request_url)
 }
+
 
 #' Get user corresponding to the specified username
 #'
@@ -52,14 +88,29 @@ get_max_item_id <- function() {
   .send_request(request_url)
 }
 
-#' Get top stories
+#' Get ids of top stories
 #'
 #' @export
 #'
 #' @return list of top stories ids
-get_top_stories <- function() {
+get_top_stories_ids <- function() {
   request_url <- create_request_url(.base_url(), c("topstories"))
   .send_request(request_url)
+}
+
+#' Get top stories
+#' @param max_items Maximum number of items to retrive. If max_items = NULL, returns all available
+#'
+#' @export
+#'
+#' @return List of top stories
+get_top_stories <- function(max_items = NULL) {
+  ids_list <- get_top_stories_ids()
+  if (!is.null(max_items))
+  {
+    ids_list <- ids_list[1:min(max_items, length(ids_list))]
+  }
+  lapply(ids_list, .get_item)
 }
 
 #' Get new stories
@@ -67,9 +118,24 @@ get_top_stories <- function() {
 #' @export
 #'
 #' @return list of new stories ids
-get_new_stories <- function() {
+get_new_stories_ids <- function() {
   request_url <- create_request_url(.base_url(), c("newstories"))
   .send_request(request_url)
+}
+
+#' Get new stories
+#' @param max_items Maximum number of items to retrive. If max_items = NULL, returns all available
+#'
+#' @export
+#'
+#' @return list of new stories
+get_new_stories <- function(max_items = NULL) {
+  ids_list <- get_new_stories_ids()
+  if (!is.null(max_items))
+  {
+    ids_list <- ids_list[1:min(max_items, length(ids_list))]
+  }
+  lapply(ids_list, .get_item)
 }
 
 #' Get best stories
@@ -77,8 +143,32 @@ get_new_stories <- function() {
 #' @export
 #'
 #' @return list of best stories ids
-get_best_stories <- function() {
+get_best_stories_ids <- function() {
   request_url <- create_request_url(.base_url(), c("beststories"))
+  .send_request(request_url)
+}
+
+#' Get best stories
+#' @param max_items Maximum number of items to retrive. If max_items = NULL, returns all available
+#' @export
+#'
+#' @return list of best stories
+get_best_stories <- function(max_items = NULL) {
+  ids_list <- get_best_stories_ids()
+  if (!is.null(max_items))
+  {
+    ids_list <- ids_list[1:min(max_items, length(ids_list))]
+  }
+  lapply(ids_list, .get_item)
+}
+
+#' Get latest ask HN stories
+#'
+#' @export
+#'
+#' @return list of latest ask HN stories ids
+get_latest_ask_stories_ids <- function() {
+  request_url <- create_request_url(.base_url(), c("askstories"))
   .send_request(request_url)
 }
 
@@ -88,8 +178,8 @@ get_best_stories <- function() {
 #'
 #' @return list of latest ask HN stories ids
 get_latest_ask_stories <- function() {
-  request_url <- create_request_url(.base_url(), c("askstories"))
-  .send_request(request_url)
+  ids_list <- get_latest_ask_stories_ids()
+  lapply(ids_list, .get_item)
 }
 
 #' Get latest show HN stories
@@ -97,9 +187,19 @@ get_latest_ask_stories <- function() {
 #' @export
 #'
 #' @return list of latest show HN stories ids
-get_latest_show_stories <- function() {
+get_latest_show_stories_ids <- function() {
   request_url <- create_request_url(.base_url(), c("showstories"))
   .send_request(request_url)
+}
+
+#' Get latest show HN stories
+#'
+#' @export
+#'
+#' @return list of latest show HN stories
+get_latest_show_stories <- function() {
+  ids_list <- get_latest_show_stories_ids()
+  lapply(ids_list, .get_item)
 }
 
 #' Get latest job stories
@@ -107,9 +207,19 @@ get_latest_show_stories <- function() {
 #' @export
 #'
 #' @return list of latest job stories ids
-get_latest_job_stories <- function() {
+get_latest_job_stories_ids <- function() {
   request_url <- create_request_url(.base_url(), c("jobstories"))
   .send_request(request_url)
+}
+
+#' Get latest job stories
+#'
+#' @export
+#'
+#' @return list of latest job stories
+get_latest_job_stories <- function() {
+  ids_list <- get_latest_job_stories_ids()
+  lapply(ids_list, .get_item)
 }
 
 
@@ -122,3 +232,5 @@ get_updates <- function() {
   request_url <- create_request_url(.base_url(), c("updates"))
   .send_request(request_url)
 }
+
+
